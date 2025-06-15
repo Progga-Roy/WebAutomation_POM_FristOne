179301ad2324a2ce8564b24f69f7b7c3992e59d7
Progga-Roy <proggaroy2000@gmail.com>
2025-06-15 18:38:07 +0600

    Create pages and find path

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class LoginPage {
+    public String url  = "https://auth.wikimedia.org/enwiki/wiki/Special:UserLogin";
+    public String user_Name = "Selenium1223";
+    public String userPass = "automation223";
+    public String userEmail = "z7x71cqy9q@qejjyl.com";
+    public String errorText = "Incorrect username or password entered. Please try again.";
+    public By userName = By.xpath("//input[@id='wpName2']");
+    public By password = By.xpath("//input[@id='wpPassword1']");
+    public By checkBox = By.xpath("//input[@id='wpRemember']");
+    public By loginBtn = By.xpath("//button[@id='wpLoginAttempt']");
+    public By errorMsg = By.xpath("//div[@class='cdx-message__content']");
+    public By name = By.xpath("(//span[contains(text(),'Selenium1223')])[1]");
+}
---
+++ src/test/java/pages/MainPage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class MainPage {
+    public  String url = "https://en.wikipedia.org/wiki/Main_Page";
+    public String title = "Wikipedia, the free encyclopedia";
+    public By mainLogo = By.xpath("//span[@class='mw-logo-container skin-invert']");
+    public By searchBox = By.xpath("//input[@title='Search Wikipedia");
+    public By searchBtn = By.xpath("//button[contains(text(),'search')");
+    public By appearance = By.xpath("//input[@id='vector-appearance-dropdown-checkbox']");
+    public By donate_btn = By.xpath("//a[@data-mw='interface']//span[contains(text(),'Donate')]");
+    public By create_account = By.xpath("//a[@data-mw='interface']//span[contains(text(),'Create account')]");
+    public By login_btn = By.xpath("//a[@data-mw='interface']//span[contains(text(),'Log in')]");
+    public By sidebar = By.xpath("//input[@id='vector-main-menu-dropdown-checkbox']");
+
+    public By name = By.xpath("(//span[contains(text(),'Selenium1223')])[1]");;
+    public By user_icon = By.xpath("//input[@id='vector-user-links-dropdown-checkbox']");
+
+}
